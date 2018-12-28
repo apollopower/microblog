84f6c35f1dd459d85077f6057bf9ddf8de0c3487
Jonas <erthaljonas@gmail.com>
2018-12-28 08:25:12 -0500

    Implemented flask-wtforms, working login form (on client side)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,32 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Snorlax!"
+    user = {'username': 'Dude'}
+    posts = [
+        {
+            'author': {'username': 'Jonas'},
+            'body': 'Rainy day in Miami!'
+        },
+        {
+            'author': {'username': 'Snorlax'},
+            'body': 'I ate too much Guava Cheesecake'
+        }
+    ]
+    return render_template(
+        'index.html', title='home', user=user, posts=posts
+        )
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for user {}, remember_me={}'.format(
+                form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
